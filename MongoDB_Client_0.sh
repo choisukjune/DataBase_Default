c111d3711b4fc30e36848625392cafd3b9f35c6f
choisukjune <sukjune.choi@gmail.com>
2023-02-02 23:35:56 +0900

    .

---
+++ MongoDB_Client_0.sh
@@ -0,0 +1,2 @@
+../Binary/mongodb/mongodb-macos-x86_64-6.0.4/bin/mongosh --port 59320 -u tjrwns -p 123qweasdzxc --authenticationDatabase admin
+#../Binary/mongodb/mongodb-macos-x86_64-6.0.4/bin/mongosh --port 59320
---
+++ MongoDB_Start.sh
@@ -0,0 +1,5 @@
+#del /q Log/0/.
+mkdir Log
+mkdir Log/0
+
+../Binary/mongodb/mongodb-macos-x86_64-6.0.4/bin/mongod --config Config/0.conf
